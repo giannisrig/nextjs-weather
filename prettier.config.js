5ed900d84d241b89b73b8961f44ad02c0ec8359e
giannisrig <giannisrig93@gmail.com>
2023-04-26 11:25:18 +0300

    Prettier & ESlint Updates

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "auto",
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
